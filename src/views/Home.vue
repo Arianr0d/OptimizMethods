752c5039f0c68285bb54703f5f8873645b15d1ee
Tatyana Komolyh <tatyana.comolyh@yandex.ru>
2021-10-04 21:28:05 +0300

    страница Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,90 @@
 <template>
   <div class="home">
-    <buttonComp :buttonName="buttonName"></buttonComp>
+      <button v-if="!openForm" @click="openForm = !openForm">Задать СЛАУ</button>
+      <form v-if="openForm">
+         <div>
+            <p>Введите количество строк m:</p>
+            <input type="text">
+         </div>
+         <div>
+            <p>Введите количество столбцов n:</p>
+            <input type="text">
+         </div>
+         <div>
+            <router-link to="\CalculateMatrix.vue">
+               <button>Сформировать систему</button>
+            </router-link>
+         </div>
+      </form> 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Button from '../components/Button.vue'
 
 export default {
   name: 'Home',
-  components: {
-    'buttonComp': Button
-  },
   data() {
      return {
-         buttonName: 'Посчитать СЛАУ'
+         openForm: false
      }
   }
 }
 </script>
+
+<style scoped lang="scss">
+   @import "/src/assets/css/function.scss";
+
+   .home {
+      justify-content: center;
+      font-size: vw(18);
+   }
+
+   form {
+      margin-top: vw(100);
+      text-align: center;
+   
+      div {
+         margin-bottom: vw(25);
+
+         display: flex;
+         flex-direction: row;
+         justify-content: center;
+
+         p {
+            width: vw(300);
+            text-align: right;
+            margin-right: vw(15);
+         }
+
+         input {
+            width: vw(100);
+            border-bottom: 1px solid #7B9299;
+            padding: vw(2) vw(10);
+         }
+         input:focus {
+            box-shadow: none;
+            border-bottom: 2px solid #7B9299;
+            outline-width: 0;
+         }
+         input:hover {
+            border-color: #7B9299;
+         }
+      }
+   }
+
+   button {
+      font-size: vw(18);
+      margin: vw(20);
+      box-shadow: none;
+      background: transparent;
+      padding: vw(10) vw(20);
+      border: 1px solid black;
+   }
+
+   button:hover {
+      background: #7B9299;
+      color: white;
+      border-color: #7B9299;
+   }
+</style>
