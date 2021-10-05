1ca3220c88af4c8d13fae0519830542b8a4018c3
Tatyana Komolyh <tatyana.comolyh@yandex.ru>
2021-10-06 00:39:41 +0300

    настроена визуализация матрицы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+     <div v-if="!openCalculate">
       <button v-if="!openForm" @click="openForm = !openForm">Задать СЛАУ</button>
       <form v-if="openForm">
          <div>
@@ -11,29 +12,38 @@
             <input v-model="n_value" type="text">
          </div>
          <div>
-            <router-link to="/calculatematrix">
-               <button type="submit" @click="Initializate()">Сформировать систему</button>
-            </router-link>
+            <button type="submit" @click="Initializate()">Сформировать систему</button>
          </div>
       </form> 
+     </div>
+     <div v-else>
+       <formcalculate></formcalculate>
+     </div>
   </div>
 </template>
 
 <script>
+import FormCalculate from '../components/FormCalculate.vue'
 
 export default {
   name: 'Home',
+  components: {
+     'formcalculate': FormCalculate
+  },
   data() {
      return {
          openForm: false,
+         openCalculate: false,
          m_value: '',
          n_value: ''
      }
   },
   methods: {
       Initializate() {
-         localStorage.m = this.m_value;
-         localStorage.n = this.n_value;
+         localStorage.m = this.m_value
+         localStorage.n = this.n_value
+
+         this.openCalculate = !this.openCalculate
      }
   }
 }
